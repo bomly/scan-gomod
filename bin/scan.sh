8fc36adfd3e1d10d28c25abf2ab979469ee75b75
kyuff <soren@sxl.dk>
2025-03-06 17:09:41 +0100

    Elaborate on scan.sh

--- bin/scan.sh
+++ bin/scan.sh
@@ -4,9 +4,13 @@ echo "Dumping environment variables..."
 echo
 env
 echo "---------------------------------"
+echo "Running scan..."
+result=$(/scan | base64)
+echo "RESULT=${result}" >> $GITHUB_OUTPUT
+echo "---------------------------------"
 echo "Content of GITHUB_OUTPUT"
 cat $GITHUB_OUTPUT
 echo "---------------------------------"
-echo "Running scan..."
-/scan
+echo "Displaying result..."
+echo $result | base64 --decode
 echo "---------------------------------"
