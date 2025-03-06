c434638f43398ceadee677fd74fbf13a38740f96
kyuff <soren@sxl.dk>
2025-03-06 17:01:57 +0100

    Fix scan script

--- bin/scan.sh
+++ bin/scan.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 echo "Dumping environment variables..."
 echo
 env
+echo "---------------------------------"
 echo "Content of GITHUB_ENV"
 cat $GITHUB_ENV
-echo
+echo "---------------------------------"
 echo "Running scan..."
 /scan
+echo "---------------------------------"
